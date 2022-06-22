e6f6dc511275ce203b6a55103cf750115fa55a99
Andrew <azehnder2@gmail.com>
2022-06-22 12:48:17 -0700

    Adding running code

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then ((r) => r.json())
+    .then ((questions) => setQuestions(questions))
+  }, []);
+
+  function handleAdd(newItem) {
+    console.log(newItem, questions)
+    setQuestions([...questions], newItem)
+    console.log(questions)
+  }
+
+  function handleDelete(deletedQuestion) {
+    const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+    setQuestions(updatedQuestions)
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" 
+      ? 
+      <QuestionForm onAddItem={handleAdd} /> 
+      : 
+      <QuestionList questions={ questions } onDeletedItem={handleDelete} />}
     </main>
   );
 }
